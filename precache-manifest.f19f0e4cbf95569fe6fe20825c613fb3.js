2c0bba1dba775933046dc19b406d644008c36020
Isaac Sheeets <icsheets97@gmail.com>
2019-11-09 12:48:30 -0700

    Updates

---
+++ precache-manifest.f19f0e4cbf95569fe6fe20825c613fb3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867de9b00c80f960d5c7d685f502c620",
+    "url": "/isheets/capstone/tree/master/slow-twitter/index.html"
+  },
+  {
+    "revision": "b9b4f2c06e311aee439b",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "9e0bfb3d79756d7c2918",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/css/main.05c1787f.chunk.css"
+  },
+  {
+    "revision": "b9b4f2c06e311aee439b",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/js/2.f27d15ab.chunk.js"
+  },
+  {
+    "revision": "9e0bfb3d79756d7c2918",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/js/main.07dadf2c.chunk.js"
+  },
+  {
+    "revision": "425c8d65d82f02775046",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/js/runtime-main.2ac36519.js"
+  },
+  {
+    "revision": "ef6e12417b43eafc91c2b91541558013",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/bad.ef6e1241.mp3"
+  },
+  {
+    "revision": "bcf1565a642a0eb5cb85eecc5f7aa7ca",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/bg.bcf1565a.svg"
+  },
+  {
+    "revision": "06b376b93ac0fba642bb0a27dcc6a825",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/click.06b376b9.mp3"
+  },
+  {
+    "revision": "b3426cabc0ae1b9f086ee1ea1e734a93",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/fail.b3426cab.mp3"
+  },
+  {
+    "revision": "65d5070ed7552056cd39c2784452a77b",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/holes_5.65d5070e.svg"
+  },
+  {
+    "revision": "1d3d03ec0cb3e47ab5c0eb185e7991b4",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/print.1d3d03ec.mp3"
+  },
+  {
+    "revision": "6ba164156b7560f57b0aa8f84480d962",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/success.6ba16415.mp3"
+  },
+  {
+    "revision": "92cc9868ba8755a8296f5a9c729a2d63",
+    "url": "/isheets/capstone/tree/master/slow-twitter/static/media/type.92cc9868.mp3"
+  }
+]);
