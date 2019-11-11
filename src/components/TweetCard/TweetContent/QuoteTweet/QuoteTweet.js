855b145242d4527b865802e309aea314b79a36cc
Isaac Sheeets <icsheets97@gmail.com>
2019-11-10 18:30:37 -0700

    react warning fix

--- src/components/TweetCard/TweetContent/QuoteTweet/QuoteTweet.js
+++ src/components/TweetCard/TweetContent/QuoteTweet/QuoteTweet.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import { useSelector } from "react-redux";
 import TweetText from './../TweetText';
 import TweetMedia from '../TweetMedia';
 
 const QuoteTweet = () => {
-    let quoteTweet = useSelector(state => state.game.curGame.curTweet.quoteTweet);
 
     return(
         <div className="quote-tweet-grid">
