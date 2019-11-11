2080117b1dd13b48a8386a15bebe1bf2f856f719
Isaac Sheeets <icsheets97@gmail.com>
2019-11-11 14:43:30 -0700

    more info content

--- src/components/Info.js
+++ src/components/Info.js
@@ -12,9 +12,10 @@ const Info = () => {
         <Fragment>
             <h2 className='info-title'>About this Site</h2>
             
-            <p className='info-p'>My name is Isaac Sheets and I developed Easy Come, Easy Go as a commentary on the usage of infinite scroll on Twitter.</p>
-            <p className='info-p'>I wanted to slow down the interaction for the user and prompt them to actually engage with the content they see.</p>
-
+            <p className='info-p'>My name is Isaac Sheets and I developed Easy Come, Easy Go as a commentary on the usage of infinite scroll on Twitter. Infinite scroll provides easy access to unlimited content and the user is never forced to take a step back and decide if they want to continue.</p>
+            <p className='info-p'>To reverse this concept, Easy Come, Easy Go slows down the interaction for the user and prompts them to actually engage with the content they see. The result is that the user can make a more intentional decision about the content the consume and the time they invest.</p>
+            <a className='info-link' href=''>&#x2197; Check out my other work</a>
+            <a className='info-link' href=''>&#x2197; Find me on LinkedIn</a>
             <button className='small-text info-close-button' onClick={() => {
                 if(soundState === true) {
                     clickSound.play();
